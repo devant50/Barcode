0fd615d6be028ca5f0fd56be3cb72f4845cfe178
WORK_PC_VM <WORK_PC_VM@TESTING.ORG>
2021-11-29 11:55:22 -0500

    Restructure

---
+++ appbarcode/src/components/body.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import react from 'react'
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+
+
+
+class Body extends React.Component {
+    
+render(){
+    return(
+        <div>
+            <h1>Body</h1>
+        </div>
+
+        )
+    }    
+
+} //Body
+
+
+export default Body;
